edb4b595902879db07abaf8cab03c243633b85e0
elberjsn <elberjsn@gmail.com>
2025-03-14 23:10:29 -0300

    modified:   src/main/java/com/elberjsn/restaurant/DataLoader.java 	modified:   src/main/resources/application-dev.properties 	modified:   src/main/resources/application.properties

--- src/main/java/com/elberjsn/restaurant/DataLoader.java
+++ src/main/java/com/elberjsn/restaurant/DataLoader.java
@@ -1,21 +1,35 @@
 package com.elberjsn.restaurant;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import com.elberjsn.restaurant.models.Restaurant;
+import com.elberjsn.restaurant.repository.RestaurantRepository;
+
 @Component
 public class DataLoader implements CommandLineRunner{
     @Autowired
     private Environment environment;
-
-
+    
+    @Autowired
+    private RestaurantRepository repository;
     @Override
     public void run(String... args) throws Exception {
-        var r= environment.getActiveProfiles()[0].equals("dev");
-        System.out.println(r);
+        String[] activeProfiles = environment.getActiveProfiles();
+        if (activeProfiles.length == 0) {
+           Restaurant r = new Restaurant();
+           r.setName("teste");
+           r.setCnpj("12345");
+           r.setPassword("12345");
 
+           repository.save(r);
+        } else {
+            System.out.println("Active profiles: " + Arrays.toString(activeProfiles));
+        }
     }
 
 }
